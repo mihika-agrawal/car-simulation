5db16fefbab882e3e713c0f6282e07b4e0c1eac9
mihika-agrawal <71880300+mihika-agrawal@users.noreply.github.com>
2020-10-04 13:24:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,6 @@ function setup() {
  speed=random(50,90)
   weight=random(400,1500);
   car= createSprite(50,200,50,50);
-  car.shapeColor("grey")
   wall= createSprite(1500,200,60,height/2);
   car.velocityX=speed;
 }
